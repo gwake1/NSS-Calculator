673ad4359551f3f2c748ea04207473685c057a77
gwake1 <gwake1@gmail.com>
2014-11-07 10:20:08 -0600

    Further Testing ideas for press and display

--- spec/test.js
+++ spec/test.js
@@ -1,6 +1,49 @@
+(function (){
+  "use strict";
+})
+
+describe("Entering numbers", function(){
+  describe("Entering the decimal number 7.89", function(){
+    it("should display 7.89", function(){
+      press('7');
+      press('.');
+      press("8");
+      press("9");
+      it("should be 7.89", function(){
+        assert(displayOutput(), "10.89");
+      });
+    });
+  });
+});
+
 describe("adding", function(){
-  it("7.89 + 1 + 2 = should be 10.89", function (){
-    assert(false);
+  describe("7.89 + 1 + 2 = should be 10.89", function (){
+      press('7');
+      press('.');
+      press("8");
+      press("9");
+      press("+");
+      press("1");
+      press("+");
+      press("2");
+      press("=");
+      it("should be 10.89", function (){
+        assert(false);
+      })
+  });
+  describe("7.89 + 1.00 + 2 = should be 10.89", function (){
+      press('7');
+      press('.');
+      press("8");
+      press("9");
+      press("+");
+      press("1.00");
+      press("+");
+      press("2");
+      press("=");
+      it("should be 10.89", function (){
+        assert(false);
+      })
   });
   it("1 + 1 = should be 2", function (){
     assert(false);
