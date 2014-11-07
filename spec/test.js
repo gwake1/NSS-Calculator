8e45eeb038c66860897c6869aab89a52713106ec
gwake1 <gwake1@gmail.com>
2014-11-07 09:56:02 -0600

    Initial description of requirements

---
+++ spec/test.js
@@ -0,0 +1,11 @@
+describe("adding", function(){
+  it("7.89 + 1 + 2 = should be 10.89", function (){
+    assert(false);
+  });
+  it("1 + 1 = should be 2", function (){
+    assert(false);
+  });
+  it(".9 + .1 = should be 1", function (){
+    assert(false);
+  });
+});
